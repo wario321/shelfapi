f65558e9c4358595428cc6bcd326d94ba3f48225
Dhannawat Sindhuseka <dhannawatsindhuseka@Dhannawats-MacBook-Pro.local>
2021-04-26 02:55:03 +0700

    cors fix 1

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ app.options('/upload/:shelf',(req,res) => {
 
 app.options('/add_shelf',(req,res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true); 
+  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //res.setHeader('Access-Control-Allow-Credentials', true); 
   res.end()
 })
 
