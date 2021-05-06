1a84126a779831eb8c770a8694612d39517cfc7b
Dhannawat Sindhuseka <dhannawatsindhuseka@Dhannawats-MacBook-Pro.local>
2021-05-06 19:09:04 +0700

    first deploy

--- index.js
+++ index.js
@@ -31,16 +31,6 @@ app.set('view engine','ejs');
 
 
 
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAOd3inufiCGU8v30HDfNxpODWxT5qDklY",
-    authDomain: "firstpro-1cca6.firebaseapp.com",
-    databaseURL: "https://firstpro-1cca6.firebaseio.com",
-    projectId: "firstpro-1cca6",
-    storageBucket: "firstpro-1cca6.appspot.com",
-    messagingSenderId: "1005061033098",
-    appId: "1:1005061033098:web:efe309d3af3fcd4a6b7173"
-};
   
 firebase.initializeApp(firebaseConfig);
 
